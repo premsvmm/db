90f281f25dcb99d6f35b433983f9cb76c4d4f19f
Premkumar <prem.kumar@razorpay.com>
2020-04-11 23:06:42 +0530

    changed version

--- cmd/root.go
+++ cmd/root.go
@@ -21,7 +21,7 @@ const (
 	folder_dir = "/src/github.com/premsvmm/db/config/"
 	file_name  = ".db"
 	extension  = "json"
-	version    = "1.0.4"
+	version    = "1.0.5"
 )
 
 // rootCmd represents the base command when called without any subcommands
