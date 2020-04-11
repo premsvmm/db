9bbc51c549910675a27048f72fd1b84505799cb4
Premkumar <prem.kumar@razorpay.com>
2020-04-11 23:41:22 +0530

    Removed logs

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/blang/semver"
 	"github.com/premsvmm/db/cmd"
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
@@ -20,16 +19,14 @@ func main() {
 func doSelfUpdate() {
 	selfupdate.EnableLog()
 	v := semver.MustParse(version)
-	fmt.Println(v)
 	latest, err := selfupdate.UpdateSelf(v, "premsvmm/db")
-	fmt.Println(latest)
 	if err != nil {
 		log.Println("Binary update failed:", err)
 		return
 	}
 	if latest.Version.Equals(v) {
 		// latest version is the same as current version. It means current binary is up to date.
-		log.Println("Current binary is the latest version", version)
+		//clog.Println("Current binary is the latest version", version)
 	} else {
 		log.Println("Successfully updated to version", latest.Version)
 		log.Println("Release note:\n", latest.ReleaseNotes)
